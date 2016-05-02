06c7a8068dfb0f717cc9d1d66681049cb5435797
Sven Bacia <sven.bacia@gmail.com>
2016-05-02 18:50:55 +0200

    updates visibility

--- JTSImageViewController/UIImage+JTSImageEffects.h
+++ JTSImageViewController/UIImage+JTSImageEffects.h
@@ -1,3 +1,4 @@
+
 /*
  File: UIImage+JTSImageEffects.h
  Abstract: This is a category of UIImage that adds methods to apply blur and tint effects to an image. This is the code you’ll want to look out to find out how to use vImage to efficiently calculate a blur.
@@ -111,7 +112,3 @@
                            maskImage:(UIImage *)maskImage;
 
 @end
-
-
-
-
